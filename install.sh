4a5ae16d6b6ed5f2560d4c8db68e8a63465e336b
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-15 23:26:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 echo -e "\n>>Instalando Qtile\c"
 trizen --noconfirm -Sy qtile rofi brightnessctl feh
-sudo cp -r qtile/.config /etc/skel/.config || sudo cp -r Qtile/.config /etc/skel/.config
-cp -r qtile/.config $HOME/.config || cp -r Qtile/.config $HOME/.config
+sudo cp -r qtile/qtile /etc/skel/.config/ || sudo cp -r Qtile/qtile /etc/skel/.config/
+cp -r qtile/qtile $HOME/.config/ || cp -r Qtile/qtile $HOME/.config/
