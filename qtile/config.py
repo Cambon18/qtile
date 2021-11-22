82f1bb1149731de6f20aba0984bb825449c9d91b
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-22 21:20:28 +0100

    Update config.py

--- qtile/config.py
+++ qtile/config.py
@@ -88,7 +88,7 @@ mouse = [
 ## Grupos
 
 groups = [Group(i) for i in [
-    "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
+    " 1:  "," 2:  ", " 3:  ", " 4  ", " 5  ", " 6  ", " 7  ", " 8  ", " 9  ", 
 ]]
 
 for i, group in enumerate(groups):
