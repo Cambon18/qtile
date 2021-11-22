8ad4a9768f063c1d4b81400638874af19272a589
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-22 21:35:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 echo -e "\n>>Instalando Qtile\c"
 trizen --noconfirm -Sy qtile rofi brightnessctl pulseadio pavucontrol feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager networkmanager-applet
-sudo mkdir /etc/skel/.config 2>/dev/null
+sudo mkdir /etc/skel/.config
 sudo cp -r qtile /etc/skel/.config
 sudo cp -r alacritty /etc/skel/.config
