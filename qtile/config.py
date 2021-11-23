c9fc89c6987d6f189058425c5df740b55af78f4b
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-23 09:48:17 +0100

    Update config.py

--- qtile/config.py
+++ qtile/config.py
@@ -12,7 +12,7 @@ from libqtile.command import lazy
 def autostart():
   home = path.expanduser('~')
   subprocess.Popen([home + '/.config/qtile/autostart.sh'])
-
+org-xinit
 ## Atajos de teclado
 
 mod = "mod4"
@@ -120,10 +120,9 @@ layouts = [
 
 screens = [
   Screen(
-    bottom=bar.Bar([
+    top=bar.Bar([
       widget.GroupBox(),
       widget.WindowName(),
-      widget.Battery()
       ], 30))
 ]
 
