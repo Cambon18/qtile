a52ebd8d677fe1a2b74067a1ae330ee9819d6e64
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-24 22:46:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter qtile rofi brightnessctl pulseaudio pavucontrol dunst feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager network-manager-applet volumeicon cbatticon
+trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter qtile rofi brightnessctl pulseaudio pavucontrol dunst feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager network-manager-applet volumeicon
 sudo systemctl enable lightdm.service
 sudo cp -r skel /etc
