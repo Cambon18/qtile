822a84794716d6bed8363b76dad566e73ef684bd
Martin Cambon Recarey <marcambonr2020@inf-211.pargapondal.inf>
2021-11-24 13:32:49 +0100

    Cambio estructura

--- install.sh
+++ install.sh
@@ -3,8 +3,4 @@
 echo -e "\n>>Instalando Qtile\c"
 trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter qtile rofi brightnessctl pulseaudio pavucontrol feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager network-manager-applet volumeicon cbatticon
 sudo systemctl enable lightdm.service
-sudo mkdir /etc/skel/.config
-sudo cp -r qtile /etc/skel/.config
-sudo cp -r alacritty /etc/skel/.config
-sudo cp -r .xprofile /etc/skel
-sudo cp -r Imágenes /etc/skel
+sudo cp -r skel /etc/skel
