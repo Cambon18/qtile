7c948f587ba6c2bfbb66b1dc7b2959e91e345fde
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-25 16:19:50 +0100

    Update config.py

--- skel/.config/qtile/config.py
+++ skel/.config/qtile/config.py
@@ -126,7 +126,7 @@ screens = [
       widget.CurrentLayoutIcon(scale=0.5),
       widget.CurrentLayout(),
       widget.Systray(),
-      widget.Battery(discharge_char='',charge_char='',full_char='',empty_char='',format='{char}  {percent:2.0%}',update_interval=1),
+      widget.Battery(discharge_char=' ',charge_char=' ',full_char=' ',empty_char=' ',format='{char}  {percent:2.0%}',update_interval=1),
       widget.Clock()
       ], 30)),
 ]
