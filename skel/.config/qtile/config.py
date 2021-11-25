a1553dc4269069ff0fece8656da1a01f4938855a
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-25 10:03:20 +0100

    Update config.py

--- skel/.config/qtile/config.py
+++ skel/.config/qtile/config.py
@@ -117,8 +117,8 @@ layouts = [
 ]
 
 ## Definimos Barra
-
-screens = [
+def pantalla():
+  return [
   Screen(
     top=bar.Bar([
       widget.GroupBox(fontsize=21),
@@ -129,17 +129,9 @@ screens = [
       widget.Battery(discharge_char='',charge_char='',full_char='',empty_char='',format='{char}  {percent:2.0%}',update_interval=1),
       widget.Clock()
       ], 30)),
-  Screen(
-    top=bar.Bar([
-      widget.GroupBox(fontsize=21),
-      widget.WindowName(),
-      widget.CurrentLayoutIcon(scale=0.5),
-      widget.CurrentLayout(),
-      widget.Systray(),
-      widget.Battery(discharge_char='',charge_char='',full_char='',empty_char='',format='{char}  {percent:2.0%}',update_interval=1),
-      widget.Clock()
-      ], 30))
-]
+  ]
+
+screens = [pantalla(),pantalla()]
 
 
 ## Variables
