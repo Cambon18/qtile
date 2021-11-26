619d89909360a877ba935b5a23ee037fb7f8b397
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-26 08:37:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter light-locker qtile picom rofi brightnessctl pulseaudio pavucontrol dunst feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager network-manager-applet volumeicon papirus-icon-theme
+trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-slick-greeter light-locker qtile picom rofi brightnessctl pulseaudio pavucontrol dunst feh brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller networkmanager network-manager-applet volumeicon papirus-icon-theme
+sudo sed -i '/#greeter-session/c greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
+sudo sed -i '/greeter-session/c greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
 sudo systemctl enable lightdm.service
 sudo cp -r skel /etc
