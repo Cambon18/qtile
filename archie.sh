a4f488f7d66b402321974cc8983510549bbe8a50
CambonOS <82413153+CambonOS@users.noreply.github.com>
2022-01-05 21:33:11 +0100

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter qtile python-pip python-psutil picom rofi brightnessctl pulseaudio pulseaudio-bluetooth pavucontrol dunst feh xscreensaver arandr brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller ristretto vlc lutris wine gamemode mangohud networkmanager networkmanager-openvpn network-manager-applet volumeicon blueman numlockx pacman-contrib matcha-gtk-theme papirus-icon-theme papirus-folders
+trizen --noconfirm -Sy xorg-server xorg-xinit lightdm lightdm-gtk-greeter qtile python-pip python-psutil picom rofi brightnessctl pulseaudio pulseaudio-bluetooth pavucontrol volctl dunst feh xscreensaver arandr brave-bin alacritty thunar gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller ristretto vlc lutris wine gamemode mangohud networkmanager networkmanager-openvpn network-manager-applet blueman numlockx pacman-contrib matcha-gtk-theme papirus-icon-theme papirus-folders
 sudo systemctl enable bluetooth.service
 sudo systemctl enable lightdm.service
 sudo papirus-folders -C green
