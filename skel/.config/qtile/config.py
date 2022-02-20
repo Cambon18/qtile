69ba9f99c963d805a0f8053f80c602dab42dbeea
CambonOS <82413153+CambonOS@users.noreply.github.com>
2022-02-20 12:33:42 +0100

    Update config.py

--- skel/.config/qtile/config.py
+++ skel/.config/qtile/config.py
@@ -53,6 +53,11 @@ keys = [
   Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")),
   Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")),
   Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
+  # Playerctl
+  Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause")),
+  Key([], "XF86AudioPause", lazy.spawn("playerctl play-pause")),
+  Key([], "XF86AudioNext", lazy.spawn("playerctl next")),
+  Key([], "XF86AudioPrev", lazy.spawn("playerctl previous")),
   # Brightness
   Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
   Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
