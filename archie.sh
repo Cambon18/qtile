ee1d0b87a3d232bea9e33f7d733656189a7befac
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-09-29 11:13:43 +0200

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -2,9 +2,9 @@
 yay --noconfirm -Sy $(cat list-packages)
 sudo systemctl enable bluetooth.service
 sudo systemctl enable cups.service
+sudo cp -rv lightdm /etc
 sudo systemctl enable lightdm.service
 sudo papirus-folders -C green
 yay --noconfirm -Rns papirus-folders
 sudo cp -rv skel /etc
 sudo touch /etc/skel/.config/picom.conf
-sudo cp -rv lightdm /etc
