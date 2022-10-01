a8a69daf4d73471c2bd9a2a166d9cec6ca57c1d6
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-10-01 15:54:15 +0200

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -3,9 +3,12 @@ yay --noconfirm -Sy $(cat list-packages)
 sudo systemctl enable bluetooth.service
 sudo systemctl enable cups.service
 sudo cp -rv lightdm /etc
+
 echo "greeter-session=lightdm-slick-greeter" >> /etc/lightdm/lightdm.conf
 echo "greeter-show-manual-login=true" >> /etc/lightdm/lightdm.conf
 echo "display-setup-script=cambonos-xrandr" >> /etc/lightdm/lightdm.conf
+echo -e "[Desktop Entry]\nName=Qtile\nComment=Qtile Session\nExec=qtile start\nType=Application\nKeywords=wm;tiling" > /usr/share/xsessions/Proba.desktop
+
 sudo systemctl enable lightdm.service
 sudo papirus-folders -C green
 yay --noconfirm -Rns papirus-folders
