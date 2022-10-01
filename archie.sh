2f43c5f12a75938ec42457e8523a87d127be9128
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-10-01 15:51:15 +0200

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -3,6 +3,9 @@ yay --noconfirm -Sy $(cat list-packages)
 sudo systemctl enable bluetooth.service
 sudo systemctl enable cups.service
 sudo cp -rv lightdm /etc
+echo "greeter-session=lightdm-slick-greeter" >> /etc/lightdm/lightdm.conf
+echo "greeter-show-manual-login=true" >> /etc/lightdm/lightdm.conf
+echo "display-setup-script=cambonos-xrandr" >> /etc/lightdm/lightdm.conf
 sudo systemctl enable lightdm.service
 sudo papirus-folders -C green
 yay --noconfirm -Rns papirus-folders
