732bed592efd0ea15ae1af4b9f10ce50051a3f02
Cambon18 <martincambon18@gmail.com>
2022-10-09 23:06:31 +0200

    modificados:     archie.sh

--- archie.sh
+++ archie.sh
@@ -9,4 +9,3 @@ yay --noconfirm -Rns papirus-folders
 sudo cp -rv skel /etc
 sudo cp -rv slick-greeter /usr/share
 sudo touch /etc/skel/.config/picom.conf
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
