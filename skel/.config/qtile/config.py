d56b8120f0b7f431e3331f61332390c02ce511f8
Cambon18 <martincambon18@gmail.com>
2022-10-12 21:34:21 +0200

    Eliminacion alertas 2º pantalla

--- skel/.config/qtile/config.py
+++ skel/.config/qtile/config.py
@@ -144,9 +144,6 @@ widgets_principal = [
 widgets_secundaria = [
       widget.GroupBox(**config,hide_unused="True",highlight_method="line",urgent_alert_method="line",this_current_screen_border="#39FF14",this_screen_border="#39FF14"),
       widget.WindowName(**config),
-      widget.CheckUpdates(**config,display_format=" Pacman: {updates} ",custom_command="checkupdates"),
-      widget.CheckUpdates(**config,display_format=" AUR: {updates} ",custom_command="yay -Qua"),
-      widget.CheckUpdates(**config,display_format=" Service: {updates} ",custom_command="systemctl list-units --failed | grep failed"),
       widget.Battery(**config,discharge_char=' ',charge_char=' ',full_char=' ',empty_char=' ',format='{char}  {percent:2.0%}',show_short_text='',update_interval=1),
       widget.Clock(**config)
 ]
