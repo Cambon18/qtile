3edf8f9c2907fa28eabe412632422b01b62435ea
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-12-26 13:53:16 +0100

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-n=$(wc -l list-packages)
+n=$(cat list-packages | wc -l)
 i=1
 while [[ $i -le $n ]]
 do
 	yay --noconfirm -Sy $(head -n $i list-packages | tail -n 1)
+	i=$(( $i + 1 ))
 done
 sudo systemctl enable bluetooth.service
 sudo systemctl enable cups.service
