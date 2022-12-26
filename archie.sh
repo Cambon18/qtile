2062921007f3de901dcb6deefbc58caa224bcaac
Cambon18 <martincambon18@gmail.com>
2022-12-26 10:51:04 +0100

    Instalacion paquete a paquete

--- archie.sh
+++ archie.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-yay --noconfirm -Sy $(cat list-packages)
+n=$(wc -l list-packages)
+i=1
+while [[ $i -le $n ]]
+do
+	yay --noconfirm -Sy $(head -n $i list-packages | tail -n 1)
+done
 sudo systemctl enable bluetooth.service
 sudo systemctl enable cups.service
 sudo cp -rv lightdm /etc
